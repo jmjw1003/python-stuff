100e8f473eb5658dae1c2d392ffc2ac99aacb30d
James Watson <jmjw1991@outlook.com>
2024-12-28 14:48:12 +0000

    Update decorators_.py

--- decorators_/decorators_.py
+++ decorators_/decorators_.py
@@ -1,6 +1,6 @@
 import time
 import logging
-from functools import wraps
+from functools import wraps, update_wrapper
 
 logging.basicConfig(level = logging.DEBUG)
 logger = logging.getLogger(__name__)
@@ -47,6 +47,47 @@ def log(func):
             raise e
     return wrapper_log
 
+"""
+Decorators with arguments
+"""
+
+def repeat(num_times):
+    def decorator_repeat(func):
+        @wraps(func)
+        def wrapper_repeat(*args, **kwargs):
+            for _ in range(num_times):
+                value = func(*args, **kwargs)
+            return value
+        return wrapper_repeat
+    return decorator_repeat
+
+
+def log_with_level(level, logger=logger):
+    def decorator_log_with_level(func):
+        @wraps(func)
+        def wrapper_log_with_level(*args, **kwargs):
+            logger.log(level, f"Running {func.__name__}")
+            return func(*args, **kwargs)
+        return wrapper_log_with_level
+    return decorator_log_with_level
+
+
+"""
+Class decorators
+"""
+
+class CountCalls:
+    def __init__(self, func):
+        update_wrapper(self, func)
+        self.func = func
+        self.num_calls = 0
+    
+    def __call__(self, *args, **kwargs):
+        value = self.func(*args, **kwargs)
+        self.num_calls += 1
+        print(f"Call {self.num_calls} of {self.func.__name__}()")
+        return value
+
 
 """
 Code to test the decorators
@@ -57,5 +98,19 @@ def foo():
     print("Hello from foo")
     raise Exception("Something went wrong")
 
+@repeat(3)
+def bar():
+    print("Hello from bar")
+
+@timer
+def baz():
+    print("Hello from baz")
+    time.sleep(3)
+
+@CountCalls
+def boo():
+    print("Hello from boo")
+
 if __name__ == "__main__":
-    foo()
+    for _ in range(3):
+        boo()
